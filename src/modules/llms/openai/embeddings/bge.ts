c5a518fe45c1e1e0284af7aa3b21f4f279916014
Developer <argen666@gmail.com>
2023-09-13 21:53:31 -0400

    embeddings bge-en

--- src/modules/llms/openai/embeddings/bge.ts
+++ src/modules/llms/openai/embeddings/bge.ts
@@ -2,7 +2,7 @@ import { Pipeline, pipeline, env } from "@xenova/transformers";
 import {Embeddings, EmbeddingsParams} from "langchain/embeddings/base";
 import path from 'path';
 //import { chunkArray } from "langchain/dist/util/chunk.js";
-//env.remoteHost = 'https://cashq-models.storage.googleapis.com'; //fixme move to config..
+//env.remoteHost = 'https://cashq-models.storage.googleapis.com'; //fixme move to config.
 env.allowRemoteModels = false;
 env.cacheDir = path.join(process.cwd(), 'cache');
 env.localModelPath = path.join(process.cwd(), 'models');
